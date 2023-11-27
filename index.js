bfa43babc197619313a132c2b26628f21c1db48d
shri234 <43914954+shri234@users.noreply.github.com>
2023-11-28 01:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/user", User);
 app.use("/ticket", ticket_details);
 app.use("/payment", payment_details);
 
-let port = process.env.PORT || 80;
+let port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`Server Has Started! at port ${port}`);
 });
