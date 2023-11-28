039d8a0c9fabce3a917732c5973107858aded22e
shri234 <43914954+shri234@users.noreply.github.com>
2023-11-29 01:26:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let express = require("express"),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser");
 cors = require("cors");
+const cron=require("node-cron")
 
 const User = require("./services/auth/auth.controller");
 const ticket_details = require("./services/ticket_details/ticket_details.contrroller");
 const payment_details = require("./services/payment_details/payment_details.controller");
+const all_ticket=require("./services/ticket_details/ticket_details.service")
 var app = express();
 
 mongoose.connect(
@@ -23,6 +25,11 @@ app.get('/', (req, res) => {
 app.use("/user", User);
 app.use("/ticket", ticket_details);
 app.use("/payment", payment_details);
+
+cron.schedule('30 1 * * *', async () => {
+  console.log('Cron job running every day at 5 PM IST');
+ await  all_ticket.updateTicket()
+});
 
 let port = process.env.PORT || 3002;
 app.listen(port, function () {
